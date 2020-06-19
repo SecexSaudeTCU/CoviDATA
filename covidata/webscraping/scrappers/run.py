26448469aa3e8767a59cbf956c5b8deacc2e052b
Monique <moniquebm@tcu.gov.br>
2020-06-19 13:45:11 -0300

    web sraping ES

--- covidata/webscraping/scrappers/run.py
+++ covidata/webscraping/scrappers/run.py
@@ -1,21 +1,23 @@
 from webscraping.scrappers.AM import uf_am
 from webscraping.scrappers.CE import uf_ce
 from webscraping.scrappers.PA import uf_pa
-from webscraping.scrappers.RJ import uf_rj_capital
+from webscraping.scrappers.RJ import rio_capital
 from webscraping.scrappers.PE import recife
 from webscraping.scrappers.AP import uf_ap
 from webscraping.scrappers.RR import uf_rr
 from webscraping.scrappers.RO import uf_ro
 from webscraping.scrappers.AC import uf_ac
+from webscraping.scrappers.ES import uf_es
 
 if __name__ == '__main__':
     uf_am.main()
     uf_ce.main()
     uf_pa.main()
-    uf_rj_capital.main()
+    rio_capital.main()
     recife.main()
     uf_ap.main()
     uf_rr.main()
     uf_ro.main()
     uf_ac.main()
+    uf_es.main()
 

9a36d8ed695de62f90bbfb2d5498eb2444cdb36f
Saul Campos Berardo <camposb@tcu.gov.br>
2020-06-23 13:50:19 -0300

    Adição de configuração automática do diretório de dados e de alguns comentários

--- covidata/config.py
+++ covidata/config.py
@@ -1,5 +1,14 @@
-diretorio_dados = 'C:\\Users\\moniq\\Documents\\TCU\\SecexSaude\\covidata\\covidata\\dados'
+"""
+Arquivo de definição de constantes e variáveis globais utilizadas em todo o projeto.
+"""
 
+import os
+
+# Define o caminho do diretório onde os dados serão armazenados
+diretorio_raiz = os.path.dirname(os.path.realpath(__file__))
+diretorio_dados = os.path.join(diretorio_raiz, 'dados')
+
+# Urls de portais de tansparência do governos estaduais
 url_pt_AM = 'http://www.transparencia.am.gov.br/covid-19/contratos/'
 url_pt_CE = 'https://cearatransparente.ce.gov.br/files/downloads/transparency/coronavirus/gasto_covid_dados_abertos.xlsx'
 url_pt_PA = 'https://transparenciacovid19.pa.gov.br/covid.json'
@@ -11,15 +20,18 @@ url_pt_ES = 'https://coronavirus.es.gov.br/Media/Coronavirus/Transparencia/Dados
 url_pt_SP = 'https://www.saopaulo.sp.gov.br/coronavirus/transparencia/'
 url_pt_AL = 'http://transparencia.al.gov.br:8080/transparencia_relatorios/transparencia_relatorios/despesa/json-despesa-covid19-itens?offset=0&tipo=xls&nome_contratado=&nota_empenho=&processo=&contrato=&objeto=&orgao=&codigo_elemento=&'
 
+# URLs de portais de transparência do governo estadual do Acre
 url_tce_AC_contratos = 'http://www.tce.ac.gov.br/covid19/Contratos-Estado.html'
 url_tce_AC_despesas = 'http://www.tce.ac.gov.br/covid19/despesa_estado_html.html'
 url_tce_AC_contratos_municipios = 'http://www.tce.ac.gov.br/covid19/Contratos-Municipios.html'
 url_tce_AC_despesas_municipios = 'http://www.tce.ac.gov.br/covid19/despesas_municipios_html.html'
 
+
+# URLSs de portais de transparência de govenos municipais (capitais)
 url_pt_Manaus = 'https://covid19.manaus.am.gov.br/transparencia-covid-19/'
 url_pt_Fortaleza = 'https://transparencia.fortaleza.ce.gov.br/index.php/despesa/despesasCovid19'
 url_pt_Belem = 'http://contratoemergencial.belem.pa.gov.br/despesas/'
-url_pt_Rio_favorecidos = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Favorecidos_Covid19_2020.xlsx'
+url_pt_Rio_favRAIZorecidos = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Favorecidos_Covid19_2020.xlsx'
 url_pt_Rio_contratos = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Contratos_Covid19_2020.xlsx'
 url_pt_Rio_despesas_por_ato = 'http://riotransparente.rio.rj.gov.br/arquivos/Open_Data_Desp_Ato_Covid19_2020.txt'
 url_pt_Recife = 'http://transparencia.recife.pe.gov.br/codigos/web/estaticos/estaticos.php?nat=COV#filho'
@@ -33,6 +45,10 @@ url_pt_Vitoria = 'http://transparencia.vitoria.es.gov.br/Licitacao.Lista.Relator
 url_pt_SaoPaulo = 'https://www.prefeitura.sp.gov.br/cidade/secretarias/controladoria_geral/transparencia_covid19/index.php?p=295874'
 url_pt_Maceio = 'http://www.licitacao.maceio.al.gov.br/covid/json' #fonte: http://www.licitacao.maceio.al.gov.br/
 
+
+# URLs dos portais de transparência dos Tribunais de Contas Municipais
 url_tcm_PA_1 = 'http://nie-tcmpa.droppages.com/files/Argus%20TCMPA%20-%20Fornecedores%20por%20Valor%20Homologado.xlsx'
 url_tcm_PA_2 = 'http://nie-tcmpa.droppages.com/files/Argus%20TCMPA%20-%20Fornecedores%20por%20Quantidade%20de%20Munic%C3%ADpios.xlsx'
 url_tcm_SP = 'https://mobile.tcm.sp.gov.br/Licitacao/ListarCovid?showMenu=0'
+
+

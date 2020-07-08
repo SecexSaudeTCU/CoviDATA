570065b45f1bf1766e8efb16c72c340f18ee948a
ericlbc <ericlb@tcu.gov.br>
2020-07-08 09:54:50 -0300

    Portal Transparência João Pessoa reimplementado como TCE-RJ

--- covidata/webscraping/scrappers/PB/uf_pb.py
+++ covidata/webscraping/scrappers/PB/uf_pb.py
@@ -2,8 +2,9 @@ import os
 from os import path
 import time
 from datetime import datetime
-from glob import glob
 import logging
+import requests
+import json
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -34,8 +35,8 @@ class PortalTransparencia_PB(SeleniumDownloader):
                                                                '//*[@id="RPTRender_ctl08_ctl00"]')))
         self.driver.execute_script("arguments[0].click();", element)
 
-        # On hold por 5 segundos
-        time.sleep(5)
+        # On hold por 8 segundos
+        time.sleep(8)
 
         # Seleciona o dropdown menu em forma de "disquete"
         self.driver.find_element_by_id('RPTRender_ctl09_ctl04_ctl00_ButtonImg').click()
@@ -78,77 +79,100 @@ class PortalTransparencia_PB(SeleniumDownloader):
         os.unlink(path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'Paraiba', 'ListaContratos.xlsx'))
 
 
-# Define a classe referida como herdeira da class "SeleniumDownloader"
-class PortalTransparencia_JoaoPessoa(SeleniumDownloader):
+def pt_JoaoPessoa():
+    """
+    Ponto de entrada do script.
+    """
 
-    # Sobrescreve o construtor da class "SeleniumDownloader"
-    def __init__(self):
-        super().__init__(path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'JoaoPessoa'),
-                         config.url_pt_JoaoPessoa)
+    # Realiza o web scraping da tabela principal do portal da transparência de João Pessoa
+    json_content = __baixa_arquivo()
 
-    # Implementa localmente o método interno e vazio da class "SeleniumDownloader"
-    def _executar(self):
-        # On hold por 5 segundos
-        time.sleep(5)
+    # Realiza o "parsing" do arquivo JSON para extração dos dados da tabela e retorna um objeto pandas DataFrame
+    df_despesas = __esquadrinha_json(json_content)
 
-        # Seleciona o botão "Pesquisar"
-        self.driver.find_element_by_xpath(
-            '/html/body/app-dashboard/div/main/div[1]/app-despesas/despesas-detalhamento/div/despesas-detalhamento-filtro/div/div/div[3]/div[2]/div/button/span[2]').click()
+    # Cria diretório do portal da transparência de João Pessoa
+    diretorio_jp = os.path.join(path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'JoaoPessoa'))
+    if not os.path.exists(diretorio_jp): os.makedirs(diretorio_jp)
 
-        # On hold por 10 segundos
-        time.sleep(10)
+    # Cria arquivo "xlsx" e aloca file handler de leitura para a variável "writer"
+    with pd.ExcelWriter(path.join(diretorio_jp, 'Dados_Portal_Transparencia_JoaoPessoa.xlsx')) as writer:
+        # Salva os dados de despesas contidos em "df_despesas" na planilha "Despesas"
+        df_despesas.to_excel(writer, sheet_name='Despesas', index=False)
 
-        # Seleciona o dropdown menu "Download"
-        self.driver.find_element_by_xpath('//*[@id="ui-accordiontab-1"]/span[2]').click()
 
-        # On hold por 3 segundos
-        time.sleep(3)
+def __baixa_arquivo():
+    """
+    Realiza o web scraping do conteúdo da tabela principal do portal e retorna o conteúdo no formato JSON.
+    """
 
-        # Seleciona a opção "XLS"
-        self.driver.find_element_by_xpath('//*[@id="ui-accordiontab-1-content"]/div/img[4]').click()
+    # URL utilizada ara obnteção de dados mostrados no painel do PT João Pessoa
+    url = 'https://transparencia.joaopessoa.pb.gov.br:8080/despesas-detalhamento'
 
-        # On hold por 15 segundos
-        time.sleep(15)
+    # Cabeçalhos da requisição POST
+    headers = {'Host': 'transparencia.joaopessoa.pb.gov.br:8080',
+               'Connection': 'keep-alive',
+               'Content-Length': '12',
+               'Accept': 'application/json, text/plain, */*',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+               'Content-Type': 'application/json',
+               'Origin': 'https://transparencia.joaopessoa.pb.gov.br',
+               'Sec-Fetch-Site': 'same-site',
+               'Sec-Fetch-Mode': 'cors',
+               'Sec-Fetch-Dest': 'empty',
+               'Referer': 'https://transparencia.joaopessoa.pb.gov.br/',
+               'Accept-Encoding': 'gzip, deflate, br',
+               'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7'}
 
-        # Obtém objeto list dos arquivos armazenados no path passado como argumento para a função nativa "glob"
-        list_files = glob(path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'JoaoPessoa/*'))
+    # Payload da requisição POST
+    payload = '{"ano":2020}'
 
-        # Obtém o primeiro elemento do objeto list que corresponde ao nome do arquivo "xlsx" baixado
-        file_name = list_files[0]
+    # Realizar a requisição POST para obtenção dos dados da tabela principal da seção  "Compras Diretas SIGA"
+    r = requests.post(url, headers=headers, data=payload.encode('utf-8'))
 
-        # Lê o arquivo de nome "file_name" de despesas baixado como um objeto pandas DataFrame selecionando as linhas e colunas úteis
-        df_despesas = pd.read_excel(
-            path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'JoaoPessoa', file_name))
+    # Obtém os dados em formato JSON com o conteúdo da tabela
+    d = json.loads(r.content)
 
-        # Elimina a coluna "ano_empenho" do objeto pandas DataFrame "df_despesas"
-        df_despesas.drop(columns='ano_empenho', inplace=True)
+    return d
 
-        # Renomeia as colunas que especifica
-        df_despesas.rename(index=str,
-                           columns={'nume_empenho': 'Empenho',
-                                    'data_empenho': 'Data Empenho',
-                                    'unidade_orcamentaria': 'Unidade',
-                                    'favorecido': 'Nome Favorecido',
-                                    'valor_empenhado': 'Valor Empenhado',
-                                    'valor_liquidado': 'Valor Liquidado',
-                                    'valor_pago': 'Valor Pago',
-                                    'saldo_pagar': 'Saldo a Pagar'},
-                           inplace=True)
 
-        # Slice da coluna de string "Data Empenho" apenas os caracteres de data (10 primeiros), em seguida...
-        # os converte para datatime, em seguida para date apenas e, por fim, para string no formato "dd/mm/aaaa"
-        df_despesas['Data Empenho'] = \
-            df_despesas['Data Empenho'].apply(
-                lambda x: datetime.strptime(x[:10], '%Y-%m-%d').date().strftime('%d/%m/%Y'))
+def __esquadrinha_json(json):
+    """
+    Realiza o "parsing" (esquadrinha) do arquivo JSON e retorna o conteúdo da tabela em Data Frame.
+    """
 
-        # Cria arquivo "xlsx" e aloca file handler de escrita para a variável "writer"
-        with pd.ExcelWriter(path.join(config.diretorio_dados, 'PB', 'portal_transparencia',
-                                      'JoaoPessoa', 'Dados_Portal_Transparencia_JoaoPessoa.xlsx')) as writer:
-            # Salva os dados de despesas contidos em "df_despesas" na planilha "Despesas"
-            df_despesas.to_excel(writer, sheet_name='Despesas', index=False)
+    # Inicializa objetos list para armazenar dados contidos no "dicionário" "json"
+    col_nume_empenho = []
+    col_data_empenho = []
+    col_unidade_orcamentaria = []
+    col_favorecido = []
+    col_valor_empenhado = []
+    col_valor_liquidado = []
+    col_valor_pago = []
+    col_saldo_pagar = []
 
-        # Deleta o arquivo "xlsx" de nome "file_name"
-        os.unlink(path.join(config.diretorio_dados, 'PB', 'portal_transparencia', 'JoaoPessoa', file_name))
+    # Aloca os valores das referidas chaves do "dicionário" "json" aos respectivos objetos list
+    for i in range(len(json)):
+        col_nume_empenho.append(json[i]['nume_empenho'])
+        col_data_empenho.append(json[i]['data_empenho'])
+        col_unidade_orcamentaria.append(json[i]['unidade_orcamentaria'])
+        col_favorecido.append(json[i]['favorecido'])
+        col_valor_empenhado.append(json[i]['valor_empenhado'])
+        col_valor_liquidado.append(json[i]['valor_liquidado'])
+        col_valor_pago.append(json[i]['valor_pago'])
+        col_saldo_pagar.append(json[i]['saldo_pagar'])
+
+    # Armazena os dados dos objetos list como um objeto pandas DataFrame
+    df = pd.DataFrame(list(zip(col_nume_empenho, col_data_empenho, col_unidade_orcamentaria, col_favorecido,
+                               col_valor_empenhado, col_valor_liquidado, col_valor_pago, col_saldo_pagar)),
+                      columns=['Empenho', 'Data Empenho', 'Unidade', 'Nome Favorecido',
+                               'Valor Empenhado', 'Valor Liquidado', 'Valor Pago', 'Saldo a Pagar'])
+
+    # Slice da coluna de string "Data Empenho" apenas os caracteres de data (10 primeiros), em seguida...
+    # os converte para datatime, em seguida para date apenas e, por fim, para string no formato "dd/mm/aaaa"
+    df['Data Empenho'] = \
+        df['Data Empenho'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d').date().strftime('%d/%m/%Y'))
+
+    return df
 
 
 def main():
@@ -159,16 +183,7 @@ def main():
     pt_PB.download()
     logger.info("--- %s segundos ---" % (time.time() - start_time))
 
-    # TODO: Erro (mesmo executando fora de run.py)
-    """
-    selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Element <span class="ui-button-text ui-clickable">...</span> is not clickable at point (297, 450). Other element would receive the click: <div class="cdk-overlay-backdrop cdk-overlay-dark-backdrop cdk-overlay-backdrop-showing"></div>
-  (Session info: headless chrome=83.0.4103.116)
-    """
-    """
     logger.info('Portal de transparência da capital...')
     start_time = time.time()
-    pt_JoaoPessoa = PortalTransparencia_JoaoPessoa()
-    pt_JoaoPessoa.download()
+    pt_JoaoPessoa()
     logger.info("--- %s segundos ---" % (time.time() - start_time))
-    """
-

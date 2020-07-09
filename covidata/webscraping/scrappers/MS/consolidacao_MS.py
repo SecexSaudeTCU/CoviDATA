918c0366cf5509f20da8bfa767d699936e371402
Monique <moniquebm@tcu.gov.br>
2020-07-09 20:50:50 -0300

    consolidação MS

---
+++ covidata/webscraping/scrappers/MS/consolidacao_MS.py
@@ -0,0 +1,59 @@
+import logging
+from os import path
+
+import pandas as pd
+
+from covidata import config
+from covidata.municipios.ibge import get_codigo_municipio_por_nome
+from covidata.persistencia import consolidacao
+from covidata.persistencia.consolidacao import consolidar_layout, salvar
+
+
+def pos_processar_despesas_capital(df):
+    df = df.rename(columns={'PROCESSO DE ORIGEM': 'NÚMERO PROCESSO'})
+    df[consolidacao.MUNICIPIO_DESCRICAO] = 'Campo Grande'
+    return df
+
+
+def __consolidar_compras_emergenciais(data_extracao):
+    dicionario_dados = {consolidacao.CONTRATADO_CNPJ: 'CPF/CNPJ', consolidacao.CONTRATADO_DESCRICAO: 'Credor',
+                        consolidacao.CONTRATANTE_DESCRICAO: 'Órgão', consolidacao.DESPESA_DESCRICAO: 'Objeto',
+                        consolidacao.MOD_APLIC_DESCRICAO: 'Modalidade', consolidacao.VALOR_CONTRATO: 'Valor'}
+    colunas_adicionais = ['Número Processo']
+    planilha_original = path.join(config.diretorio_dados, 'MS', 'portal_transparencia',
+                                  'ComprasEmergenciaisMS_COVID19.csv')
+    df_original = pd.read_csv(planilha_original, sep=';', header=2, index_col=False)
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + config.url_pt_MS
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_ESTADUAL,
+                           fonte_dados, 'MS', '', data_extracao)
+    df[consolidacao.FAVORECIDO_TIPO] = consolidacao.TIPO_FAVORECIDO_CNPJ
+    return df
+
+
+def __consolidar_despesas_capital(data_extracao):
+    dicionario_dados = {consolidacao.CONTRATANTE_DESCRICAO: 'Órgão', consolidacao.UG_DESCRICAO: 'Unidade',
+                        consolidacao.CONTRATADO_DESCRICAO: 'Fornecedor',
+                        consolidacao.ELEMENTO_DESPESA_DESCRICAO: 'Elemento Despesa',
+                        consolidacao.VALOR_EMPENHADO: 'Total Empenhado',
+                        consolidacao.VALOR_LIQUIDADO: 'Total Liquidado', consolidacao.VALOR_PAGO: 'Total Pago'}
+    # TODO Renomear Processo de Origem para NÚMERO PROCESSO
+    colunas_adicionais = ['Processo de Origem', 'Data', 'Categoria', 'Status']
+    planilha_original = path.join(config.diretorio_dados, 'MS', 'portal_transparencia', 'Campo Grande',
+                                  'Despesas – Transparência Covid19 – Prefeitura de Campo Grande.xlsx')
+    df_original = pd.read_excel(planilha_original, header=1)
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + config.url_pt_CampoGrande
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_MUNICIPAL,
+                           fonte_dados, 'MS', get_codigo_municipio_por_nome('Campo Grande', 'MS'), data_extracao,
+                           pos_processar_despesas_capital)
+    return df
+
+
+def consolidar(data_extracao):
+    logger = logging.getLogger('covidata')
+    logger.info('Iniciando consolidação dados Mato Grosso do Sul')
+
+    compras_emergenciais = __consolidar_compras_emergenciais(data_extracao)
+    despesas_capital = __consolidar_despesas_capital(data_extracao)
+    compras_emergenciais = compras_emergenciais.append(despesas_capital)
+
+    salvar(compras_emergenciais, 'MS')
--- covidata/webscraping/scrappers/MS/uf_ms.py
+++ covidata/webscraping/scrappers/MS/uf_ms.py
@@ -1,4 +1,6 @@
+import datetime
 import logging
+import os
 import time
 from os import path
 
@@ -7,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from covidata import config
+from covidata.webscraping.scrappers.MS.consolidacao_MS import consolidar
 from covidata.webscraping.selenium.downloader import SeleniumDownloader
 
 
@@ -30,11 +33,15 @@ class PortalTransparencia_CampoGrande(SeleniumDownloader):
     def _executar(self):
         wait = WebDriverWait(self.driver, 30)
 
+        # Aguarda o carregamento completo da página.
+        time.sleep(5)
+
         element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'buttons-excel')))
         self.driver.execute_script("arguments[0].click();", element)
 
 
 def main():
+    data_extracao = datetime.datetime.now()
     logger = logging.getLogger('covidata')
     logger.info('Portal de transparência estadual...')
     start_time = time.time()
@@ -47,8 +54,17 @@ def main():
     logger.info('Portal de transparência estadual...')
     start_time = time.time()
 
+    # Renomeia o arquivo
+    diretorio = path.join(config.diretorio_dados, 'MS', 'portal_transparencia')
+    arquivo = os.listdir(diretorio)[0]
+    os.rename(path.join(diretorio, arquivo), path.join(diretorio, 'ComprasEmergenciaisMS_COVID19.csv'))
+
     pt_CampoGrande = PortalTransparencia_CampoGrande()
     pt_CampoGrande.download()
 
     logger.info("--- %s segundos ---" % (time.time() - start_time))
 
+    logger.info('Consolidando as informações no layout padronizado...')
+    start_time = time.time()
+    consolidar(data_extracao)
+    logger.info("--- %s segundos ---" % (time.time() - start_time))

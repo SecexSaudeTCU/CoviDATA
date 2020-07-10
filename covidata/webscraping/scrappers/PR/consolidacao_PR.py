938191ddae472c17cd3b1ad628cfd524f318c239
Monique <moniquebm@tcu.gov.br>
2020-07-10 19:50:09 -0300

    consolidação PR

---
+++ covidata/webscraping/scrappers/PR/consolidacao_PR.py
@@ -0,0 +1,119 @@
+import datetime
+import logging
+from os import path
+
+import pandas as pd
+
+from covidata import config
+from covidata.municipios.ibge import get_codigo_municipio_por_nome
+from covidata.persistencia import consolidacao
+from covidata.persistencia.consolidacao import consolidar_layout, salvar
+
+
+def pos_processar_dados_abertos(df):
+    df[consolidacao.ANO] += 2000
+    df[consolidacao.TIPO_DOCUMENTO] = 'Empenho'
+    return df
+
+
+def posprocessar_aquisicoes_capital(df):
+    df[consolidacao.MUNICIPIO_DESCRICAO] = 'Curitiba'
+    df[consolidacao.TIPO_DOCUMENTO] = 'Empenho'
+    return df
+
+
+def __consolidar_aquisicoes(data_extracao, url_fonte_dados):
+    dicionario_dados = {consolidacao.CONTRATANTE_DESCRICAO: 'ORGÃO - CONTRATANTE/MUNICÍPIO',
+                        consolidacao.UG_DESCRICAO: 'ORGÃO - CONTRATANTE/MUNICÍPIO',
+                        consolidacao.CONTRATADO_DESCRICAO: 'EMPRESA CONTRATADA/CNPJ ',
+                        consolidacao.DESPESA_DESCRICAO: 'OBJETO',
+                        consolidacao.ITEM_EMPENHO_QUANTIDADE: 'QUANTIDADE POR UNIDADES / DIÁRIAS',
+                        consolidacao.ITEM_EMPENHO_VALOR_UNITARIO: 'VALOR    UNITÁRIO (R$)',
+                        consolidacao.ITEM_EMPENHO_VALOR_TOTAL: 'VALOR                          TOTAL (R$)'}
+    colunas_adicionais = ['DATA  PUBLICAÇÃO                       DIOE / FOLHA', 'PRAZO              CONTRATO (mês)',
+                          'NÚMERO DISPENSA', 'PROTOCOLO / PROCESSO']
+    planilha_original = path.join(config.diretorio_dados, 'PR', 'portal_transparencia',
+                                  'aquisicoes_e_contratacoes_0.xls')
+    df_original = pd.read_excel(planilha_original, header=6, sheet_name='UNIFICAÇÃO DOS 4 MESES')
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + url_fonte_dados
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_ESTADUAL,
+                           fonte_dados, 'PR', '', data_extracao)
+    return df
+
+
+def __consolidar_aquisicoes_capital(data_extracao):
+    dicionario_dados = {consolidacao.DOCUMENTO_DATA: 'Data', consolidacao.DOCUMENTO_NUMERO: 'Documento/Empenho',
+                        consolidacao.FUNCAO_DESCRICAO: 'Função', consolidacao.SUBFUNCAO_DESCRICAO: 'Subfunção',
+                        consolidacao.FONTE_RECURSOS_DESCRICAO: 'Fonte', consolidacao.MOD_APLIC_DESCRICAO: 'Modalidade',
+                        consolidacao.ELEMENTO_DESPESA_DESCRICAO: 'Elemento',
+                        consolidacao.CONTRATANTE_DESCRICAO: 'Órgão', consolidacao.UG_DESCRICAO: 'Órgão',
+                        consolidacao.VALOR_CONTRATO: 'Valor R$'}
+    colunas_adicionais = ['Categoria', 'Grupo']
+    planilha_original = path.join(config.diretorio_dados, 'PR', 'portal_transparencia', 'Curitiba', 'aquisicoes.xlsx')
+    df_original = pd.read_excel(planilha_original, header=7)
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + config.url_pt_Curitiba_aquisicoes
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_MUNICIPAL,
+                           fonte_dados, 'PR', get_codigo_municipio_por_nome('Curitiba', 'PR'), data_extracao,
+                           posprocessar_aquisicoes_capital)
+    return df
+
+#TODO: TESTAR
+def __consolidar_licitacoes_capital(data_extracao):
+    dicionario_dados = {consolidacao.CONTRATANTE_DESCRICAO: 'ÓRGÃO', consolidacao.UG_DESCRICAO: 'ÓRGÃO',
+                        consolidacao.DESPESA_DESCRICAO: 'OBJETO',
+                        consolidacao.MOD_APLIC_DESCRICAO: 'MODALIDADE DA CONTRATAÇÃO',
+                        consolidacao.MOD_APLICACAO_COD: 'Nº DA MODALIDADE',
+                        consolidacao.CONTRATADO_DESCRICAO: 'CONTRATADO (s)', consolidacao.CONTRATADO_CNPJ: 'CNPJ',
+                        consolidacao.ITEM_EMPENHO_QUANTIDADE: 'QUANTIDADE',
+                        consolidacao.ITEM_EMPENHO_VALOR_UNITARIO: ' VALOR UNITÁRIO ',
+                        consolidacao.ITEM_EMPENHO_VALOR_TOTAL: 'VALOR TOTAL/GLOBAL',
+                        consolidacao.VALOR_EMPENHADO: 'EMPENHADO', consolidacao.DOCUMENTO_NUMERO: 'EMPENHO Nº ',
+                        consolidacao.VALOR_LIQUIDADO: 'VALOR LIQUIDADO', consolidacao.VALOR_PAGO: 'VALOR PAGO',
+                        consolidacao.FONTE_RECURSOS_DESCRICAO: 'FONTE DE RECURSOS'}
+    colunas_adicionais = ['LOCAL DA EXECUÇÃO/ENTREGA', 'PUBLICAÇÃO NO DIÁRIO OFICIAL', 'PROTOCOLO', 'Nº CONTRATO',
+                          'VIGENCIA DO CONTRATO', 'VALOR CANCELADO', 'BOLETIM DE PAGAMENTO', 'A PAGAR']
+    planilha_original = path.join(config.diretorio_dados, 'PR', 'portal_transparencia', 'Curitiba',
+                                  'licitacoes_contratacoes.csv')
+    df_original = pd.read_csv(planilha_original)
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + config.url_pt_Curitiba_contratacoes
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_MUNICIPAL,
+                           fonte_dados, 'PR', get_codigo_municipio_por_nome('Curitiba', 'PR'), data_extracao)
+    return df
+
+
+def __consolidar_dados_abertos(data_extracao, url_fonte_dados):
+    dicionario_dados = {consolidacao.ANO: 'ANO', consolidacao.DOCUMENTO_NUMERO: 'EMPENHO',
+                        consolidacao.FUNCAO_COD: 'FUNÇÃO', consolidacao.SUBFUNCAO_COD: 'SUBFUNÇÃO',
+                        consolidacao.PROGRAMA_COD: 'PROGRAMA', consolidacao.MOD_APLICACAO_COD: 'MODALIDADE',
+                        consolidacao.ELEMENTO_DESPESA_COD: 'ELEMENTO',
+                        consolidacao.SUB_ELEMENTO_DESPESA_COD: 'SUBELEMENTO', consolidacao.FONTE_RECURSOS_COD: 'FONTE',
+                        consolidacao.VALOR_EMPENHADO: 'EMPENHADO', consolidacao.VALOR_LIQUIDADO: 'LIQUIDADO',
+                        consolidacao.VALOR_PAGO: 'PAGO'}
+    colunas_adicionais = ['CONTA CORRENTE', 'MÊS', 'PODER', 'UNIDADE CONTÁBIL', 'UNIDADE ORÇAMENTÁRIA', 'P_A_OE',
+                          'NATUREZA', 'NATUREZA2', 'CATEGORIA', 'ESPECIE', 'OBRA_META', 'HISTORICO', 'Modalidade2']
+    planilha_original = path.join(config.diretorio_dados, 'PR', 'portal_transparencia', 'dados_abertos.xlsx')
+    df_original = pd.read_excel(planilha_original)
+    fonte_dados = consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + url_fonte_dados
+    df = consolidar_layout(colunas_adicionais, df_original, dicionario_dados, consolidacao.ESFERA_ESTADUAL,
+                           fonte_dados, 'PR', '', data_extracao, pos_processar_dados_abertos)
+    return df
+
+
+def consolidar(data_extracao, url_fonte_aquisicoes, url_fonte_dados_abertos):
+    logger = logging.getLogger('covidata')
+    logger.info('Iniciando consolidação dados Paraná')
+
+    aquisicoes = __consolidar_aquisicoes(data_extracao, url_fonte_aquisicoes)
+
+    dados_abertos = __consolidar_dados_abertos(data_extracao, url_fonte_dados_abertos)
+    aquisicoes = aquisicoes.append(dados_abertos)
+
+    aquisicoes_capital = __consolidar_aquisicoes_capital(data_extracao)
+    aquisicoes = aquisicoes.append(aquisicoes_capital)
+
+    salvar(aquisicoes, 'PR')
+
+
+consolidar(datetime.datetime.now(),
+           'http://www.coronavirus.pr.gov.br/sites/cadastrocovid19/arquivos_restritos/files/documento/2020-06/aquisicoes_e_contratacoes_0.xls',
+           'http://www.coronavirus.pr.gov.br/sites/cadastrocovid19/arquivos_restritos/files/documento/2020-07/dados_abertos.xlsx')
--- covidata/webscraping/scrappers/PR/uf_pr.py
+++ covidata/webscraping/scrappers/PR/uf_pr.py
@@ -1,7 +1,10 @@
 import datetime
 import logging
+import os
 import time
 from os import path
+
+import win32com.client
 
 from covidata import config
 from covidata.webscraping.downloader import FileDownloader
@@ -17,17 +20,22 @@ class PortalTransparencia_Curitiba(SeleniumDownloader):
         button = self.driver.find_element_by_class_name('excel')
         button.click()
 
+
 def portal_transparencia_PR():
     agora = datetime.datetime.now()
     mes_atual = agora.month
+    mes_anterior = mes_atual - 1
 
     if mes_atual <= 9:
         mes_atual = '0' + str(mes_atual)
 
+    if mes_anterior <= 9:
+        mes_anterior = '0' + str(mes_anterior)
+
     ano_atual = agora.year
 
     nome_arquivo_aquisicoes = 'aquisicoes_e_contratacoes_0.xls'
-    url = f'{config.url_pt_PR}{ano_atual}-{mes_atual}/{nome_arquivo_aquisicoes}'
+    url = f'{config.url_pt_PR}{ano_atual}-{mes_anterior}/{nome_arquivo_aquisicoes}'
 
     pt_PR_aquisicoes = FileDownloader(path.join(config.diretorio_dados, 'PR', 'portal_transparencia'), url,
                                       nome_arquivo_aquisicoes)
@@ -43,11 +51,12 @@ def portal_transparencia_PR():
 
 def portal_transparencia_Curitiba():
     pt_contratacoes = FileDownloader(path.join(config.diretorio_dados, 'PR', 'portal_transparencia', 'Curitiba'),
-                                 config.url_pt_Curitiba_contratacoes, 'licitacoes_contratacoes.csv')
+                                     config.url_pt_Curitiba_contratacoes, 'licitacoes_contratacoes.csv')
     pt_contratacoes.download()
 
     pt_aquisicoes = PortalTransparencia_Curitiba()
     pt_aquisicoes.download()
+
 
 def main():
     logger = logging.getLogger('covidata')
@@ -63,5 +72,32 @@ def main():
 
     portal_transparencia_Curitiba()
 
+    __exportar_arquivo_para_xlsx(path.join(config.diretorio_dados, 'PR', 'portal_transparencia', 'Curitiba'),
+                       'Aquisições_para_enfrentamento_da_pandemia_do_COVID-19_-_Transparência_Curitiba.xls',
+                       'aquisicoes.xlsx')
+
     logger.info("--- %s segundos ---" % (time.time() - start_time))
 
+#TODO: Solução que só funciona no Windows!
+def __exportar_arquivo_para_xlsx(diretorio, nome_arquivo_original, nome_novo_arquivo):
+    """
+    Função que só funciona no Windows, a ser utilizada exepcionalmente para arquivos XLS corruptos.  Esses arquivos não
+    não abrem com o uso de bibliotecas Python convencionais. A única solução até o momento é salvar o arquivo no
+    formato XLSX.
+    :param diretorio: Diretório de origem e de destino.
+    :param nome_arquivo_original: Nome do arquivo original.
+    :param nome_novo_arquivo: Nome do arquivo a ser gerado.
+    :return:
+    """
+    App = win32com.client.Dispatch("Excel.Application")
+    App.Visible = False
+    caminho_arquivo_anterior = path.join(diretorio, nome_arquivo_original)
+    workbook = App.Workbooks.Open(caminho_arquivo_anterior)
+    workbook.ActiveSheet.SaveAs(path.join(diretorio, nome_novo_arquivo), 51)  # 51 is for xlsx
+    workbook.Close(SaveChanges=True)
+    App.Quit()
+
+    #Remove o arquivo anterior.
+    os.remove(caminho_arquivo_anterior)
+
+main()

c228e2de9375ac102b4d796c445e7f306e345b15
ericlbc <ericlb@tcu.gov.br>
2020-07-18 15:51:48 -0300

    Decodificação PT Acre e respectiva consolidação

--- covidata/webscraping/scrappers/AC/consolidacao_AC.py
+++ covidata/webscraping/scrappers/AC/consolidacao_AC.py
@@ -85,6 +85,18 @@ def pos_processar_dispensas_municipios(df):
     return df
 
 
+def pos_processar_portal_transparencia_estadual(df):
+    for i in range(len(df)):
+        cpf_cnpj = df.loc[i, consolidacao.CONTRATADO_CNPJ]
+
+        if len(str(cpf_cnpj)) >= 14:
+            df.loc[i, consolidacao.FAVORECIDO_TIPO] = consolidacao.TIPO_FAVORECIDO_CNPJ
+        else:
+            df.loc[i, consolidacao.FAVORECIDO_TIPO] = 'CPF/RG'
+
+    return df
+
+
 def pos_processar_portal_transparencia_capital(df):
     # Elimina as sete últimas linhas
     df.drop(df.tail(7).index, inplace=True)
@@ -190,6 +202,57 @@ def __consolidar_dispensas_municipios(data_extracao):
     return df
 
 
+def __consolidar_portal_transparencia_estadual(data_extracao):
+
+    # Objeto dict em que os valores tem chaves que retratam campos considerados mais importantes
+    dicionario_dados = {consolidacao.CONTRATANTE_DESCRICAO: 'Órgão',
+                        consolidacao.VALOR_EMPENHADO: 'Valor Empenhado',
+                        consolidacao.NUMERO_CONTRATO: 'Número Contrato',
+                        consolidacao.CONTRATADO_CNPJ: 'CNPJ',
+                        consolidacao.CONTRATADO_DESCRICAO: 'Nome Contratada',
+                        consolidacao.DATA_INICIO_VIGENCIA: 'Data Início Vigência',
+                        consolidacao.DATA_FIM_VIGENCIA: 'Data Fim Vigência',
+                        consolidacao.DOCUMENTO_NUMERO: 'Número Empenho',
+                        consolidacao.DOCUMENTO_DATA: 'Data Empenho',
+                        consolidacao.ACAO_COD: 'Código Ação',
+                        consolidacao.ACAO_DESCRICAO: 'Descrição Ação'}
+
+    # Objeto list cujos elementos retratam campos não considerados tão importantes (for now at least)
+    colunas_adicionais = ['Situação Contratual']
+
+    # Lê o arquivo "csv" de empenhos baixado como um objeto pandas DataFrame
+    df_original = pd.read_csv(
+        path.join(config.diretorio_dados, 'AC', 'portal_transparencia', 'empenhos.csv'))
+
+    # Remove notação científica das colunas especificadas do objeto pandas DataFrame "df_original"
+    df_original['11'] = df_original['11'].apply(lambda x: '%.0f' % x)
+
+    # Seleciona as colunas de "df_original" a integrar "df"
+    df = df_original[['1', '2', '6', '7', '8', '9', '10', '11', '12', '16', '17']]
+
+    # Renomeia as colunas de "df" para nomes inteligíveis
+    df.rename(columns={'1': 'Órgão',
+                       '2': 'Situação Contratual',
+                       '5': 'Valor Empenhado',
+                       '6': 'Número Contrato',
+                       '7': 'CNPJ',
+                       '8': 'Nome Contratada',
+                       '9': 'Data Início Vigência',
+                       '10': 'Data Fim Vigência',
+                       '11': 'Número Empenho',
+                       '12': 'Data Empenho',
+                       '16': 'Código Ação',
+                       '17': 'Descrição Ação'},
+              inplace=True)
+
+    # Chama a função "consolidar_layout" definida em módulo importado
+    df = consolidar_layout(colunas_adicionais, df, dicionario_dados, consolidacao.ESFERA_ESTADUAL,
+                           consolidacao.TIPO_FONTE_PORTAL_TRANSPARENCIA + ' - ' + config.url_pt_AC, 'AC', '',
+                           data_extracao, pos_processar_portal_transparencia_estadual)
+
+    return df
+
+
 def __consolidar_portal_transparencia_capital(data_extracao):
     dicionario_dados = {consolidacao.ANO: 'Exercício', consolidacao.CONTRATADO_DESCRICAO: 'Fornecedor',
                         consolidacao.DESPESA_DESCRICAO: 'Objeto', consolidacao.MOD_APLIC_DESCRICAO: 'Modalidade',
@@ -214,23 +277,26 @@ def consolidar(data_extracao):
     despesas = __consolidar_despesas(data_extracao)
 
     despesas_municipios = __consolidar_despesas_municipios(data_extracao)
-    despesas = despesas.append(despesas_municipios)
+    despesas = despesas.append(despesas_municipios, ignore_index=True, sort=False)
 
     despesas.fillna('')
 
     contratos = __consolidar_contratos(data_extracao)
-    despesas = despesas.append(contratos)
+    despesas = despesas.append(contratos, ignore_index=True, sort=False)
 
     contratos_municipios = __consolidar_contratos_municipios(data_extracao)
-    despesas = despesas.append(contratos_municipios)
+    despesas = despesas.append(contratos_municipios, ignore_index=True, sort=False)
 
     dispensas = __consolidar_dispensas(data_extracao)
-    despesas = despesas.append(dispensas)
+    despesas = despesas.append(dispensas, ignore_index=True, sort=False)
 
     dispensas_municipios = __consolidar_dispensas_municipios(data_extracao)
-    despesas = despesas.append(dispensas_municipios)
+    despesas = despesas.append(dispensas_municipios, ignore_index=True, sort=False)
+
+    pt_estadual = __consolidar_portal_transparencia_estadual(data_extracao)
+    despesas = despesas.append(pt_estadual, ignore_index=True, sort=False)
 
     pt_capital = __consolidar_portal_transparencia_capital(data_extracao)
-    despesas = despesas.append(pt_capital)
+    despesas = despesas.append(pt_capital, ignore_index=True, sort=False)
 
     salvar(despesas, 'AC')

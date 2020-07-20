8b13f895cc7b5308fa30e567bb0dc3f1515323fd
Monique <moniquebm@tcu.gov.br>
2020-07-20 18:34:03 -0300

    desabilitando portais/código instáveis

--- covidata/webscraping/scrappers/MT/uf_mt.py
+++ covidata/webscraping/scrappers/MT/uf_mt.py
@@ -16,14 +16,12 @@ from covidata.webscraping.downloader import download
 from covidata.webscraping.scrappers.MT.consolidacao_MT import consolidar
 
 
-
 class PortalTransparencia_MT(SeleniumDownloader):
     def __init__(self):
         super().__init__(path.join(config.diretorio_dados, 'MT', 'portal_transparencia'),
                          config.url_pt_MT)
 
     def _executar(self):
-
         # Cria um objeto da class "WebDriverWait"
         wait = WebDriverWait(self.driver, 45)
 
@@ -43,43 +41,14 @@ class PortalTransparencia_MT(SeleniumDownloader):
         # Seleciona a opção "Excel"
         self.driver.find_element_by_xpath('/html/body/div/div[5]/ul/li[2]/a').click()
 
-
-
-def pt_Cuiaba():
-    page = requests.get(config.url_pt_Cuiaba)
-    soup = BeautifulSoup(page.content, 'html5lib')
-
-    baixar_arquivo(soup, descricao='PLANILHA INFORMATIVA SOBRE OS GASTOS EMERGENCIAIS - SMASDH',
-                   nome_arquivo='planilha_gastos_emergenciais_SMASDH.pdf')
-    baixar_arquivo(soup, descricao='PLANILHA INFORMATIVA SOBRE OS GASTOS EMERGENCIAIS - SMS ',
-                   nome_arquivo='planilha_gastos_emergenciais_SMS.pdf')
-    baixar_arquivo(soup, descricao='PLANILHA INFORMATIVA SOBRE OS GASTOS EMERGENCIAIS - SMG - SME',
-                   nome_arquivo='planilha_gastos_emergenciais_SMSU_SME_SMGE.pdf')
-    baixar_arquivo(soup, descricao='RECURSOS RECEBIDOS E APLICADOS ', nome_arquivo='recursos_recebidos_aplicados.pdf')
-    baixar_arquivo(soup, descricao='RELAÇÃO POR ITEM ', nome_arquivo='descritivos_aquisicoes.pdf')
-
-
-def baixar_arquivo(soup, descricao, nome_arquivo):
-    span = soup.find_all('span', string=descricao)[0]
-    link = span.parent.find_all('a')[0].attrs['href']
-    url = 'http://covid.cuiaba.mt.gov.br/' + link
-    diretorio = os.path.join(config.diretorio_dados, 'MT', 'portal_transparencia', 'Cuiabá')
-    download(url, diretorio, os.path.join(diretorio, nome_arquivo))
-
-
 def main():
     data_extracao = datetime.datetime.now()
     logger = logging.getLogger('covidata')
-    
+
     logger.info('Portal de transparência estadual...')
     start_time = time.time()
     pt_MT = PortalTransparencia_MT()
     pt_MT.download()
-    logger.info("--- %s segundos ---" % (time.time() - start_time))
-
-    logger.info('Portal de transparência da capital...')
-    start_time = time.time()
-    pt_Cuiaba()
     logger.info("--- %s segundos ---" % (time.time() - start_time))
 
     logger.info('Consolidando as informações no layout padronizado...')

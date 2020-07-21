e641fbfca442383515c75d7e919bc0ba13d0f2ba
ericlbc <ericlb@tcu.gov.br>
2020-07-21 08:16:32 -0300

    Scraper TCE-BA

--- covidata/webscraping/scrappers/BA/uf_ba.py
+++ covidata/webscraping/scrappers/BA/uf_ba.py
@@ -1,14 +1,23 @@
-import datetime
+from os import path
 import logging
 import time
-
-import pandas as pd
+import datetime
 import requests
+
 from bs4 import BeautifulSoup
+import pandas as pd
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 from covidata import config
 from covidata.persistencia.dao import persistir
 from covidata.webscraping.scrappers.BA.consolidacao_BA import consolidar
+from covidata.webscraping.selenium.downloader import SeleniumDownloader
+
+
 
 def pt_BA():
     page = requests.get(config.url_pt_BA)
@@ -39,16 +48,42 @@ def pt_BA():
     persistir(df, 'portal_transparencia', 'contratos', 'BA')
 
 
+class TCE_BA(SeleniumDownloader):
+    def __init__(self):
+        super().__init__(path.join(config.diretorio_dados, 'BA', 'tce'),
+                         config.url_tce_BA,
+                         browser_option='--start-maximized')
+
+    def _executar(self):
+
+        # Cria um objeto da class "WebDriverWait"
+        wait = WebDriverWait(self.driver, 10)
+
+        # Não perfeitamente explicável por que o código funciona com essas linhas
+        try:
+            element = wait.until(EC.visibility_of_element_located((By.XPATH,
+                '//*[@id="conteudo"]/div/div/div/article/div/div[3]/ul[1]/li[7]/a')))
+            self.driver.execute_script("arguments[0].click();", element)
+        except:
+            pass
+
+
 def main():
     data_extracao = datetime.datetime.now()
     logger = logging.getLogger('covidata')
+
     logger.info('Portal de transparência estadual...')
     start_time = time.time()
     pt_BA()
+    logger.info("--- %s segundos ---" % (time.time() - start_time))
+
+    logger.info('Tribunal de Contas estadual...')
+    start_time = time.time()
+    tce_BA = TCE_BA()
+    tce_BA.download()
     logger.info("--- %s segundos ---" % (time.time() - start_time))
 
     logger.info('Consolidando as informações no layout padronizado...')
     start_time = time.time()
     consolidar(data_extracao)
     logger.info("--- %s segundos ---" % (time.time() - start_time))
-

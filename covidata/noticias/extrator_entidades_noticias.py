4a70ef43a97c456436a0822a60ad25eb35afff68
Monique <moniquebm@tcu.gov.br>
2020-08-21 19:07:18 -0300

    rotulagem para NER

--- covidata/noticias/extrator_entidades_noticias.py
+++ covidata/noticias/extrator_entidades_noticias.py
@@ -2,6 +2,7 @@ import logging
 import os
 import time
 
+import jsonlines
 import pandas as pd
 
 from covidata.noticias.gnews import executar_busca
@@ -13,33 +14,40 @@ def get_NERs():
     from covidata.noticias.ner.bert.bert_ner import BaseBERT_NER
     from covidata.noticias.ner.polyglot.polyglot_ner import PolyglotNER
 
-    return [BaseBERT_NER(), SpacyNER(True), PolyglotNER(True)]
+    return [SpacyNER(True), PolyglotNER(True), BaseBERT_NER()]
 
 
-if __name__ == '__main__':
+def extrair_entidades():
     logger = logging.getLogger('covidata')
-    logger.setLevel(logging.INFO)
-    logger.addHandler(logging.StreamHandler())
+    df = pd.read_excel('com_textos_baseline.xlsx')
 
-    start_time = time.time()
-    logger.info('Buscando as notícias na Internet...')
-    arquivo_noticias = executar_busca()
-    df = recuperar_textos(arquivo_noticias)
-    logger.info("--- %s minutos ---" % ((time.time() - start_time) / 60))
-
-    start_time = time.time()
     logger.info('Extraindo entidades relevantes das notícias...')
     ners = get_NERs()
-
     writer = pd.ExcelWriter(os.path.join('ner.xlsx'), engine='xlsxwriter')
-
     for ner in ners:
         algoritmo = ner.__class__.__name__
         logger.info('Aplicando implementação ' + algoritmo)
         start_time = time.time()
         df_resultado = ner.extrair_entidades(df)
+        ner.avaliar()
         logger.info("--- %s segundos ---" % (time.time() - start_time))
         df_resultado.to_excel(writer, sheet_name=algoritmo)
-
     writer.save()
     logger.info('Processamento concluído.')
+
+
+def obter_textos():
+    logger = logging.getLogger('covidata')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+    start_time = time.time()
+    logger.info('Buscando as notícias na Internet...')
+    arquivo_noticias = executar_busca()
+    recuperar_textos(arquivo_noticias)
+    logger.info("--- %s minutos ---" % ((time.time() - start_time) / 60))
+
+
+if __name__ == '__main__':
+    # obter_textos()
+
+    extrair_entidades()
--- covidata/noticias/ner/spacy/spacy_ner.py
+++ covidata/noticias/ner/spacy/spacy_ner.py
@@ -1,14 +1,19 @@
+import jsonlines
 import pandas as pd
 import pt_core_news_sm
+from spacy.gold import GoldParse
 
 from covidata.noticias.ner.ner_base import NER
 
 
 class SpacyNER(NER):
-    def __init__(self, filtrar_contratados=False):
+    def __init__(self, filtrar_contratados=False, modo_avaliacao=True):
         super().__init__(filtrar_contratados)
         self.nlp = pt_core_news_sm.load()
         self.map_labels = {'MISC': 'MISCELÂNEA', 'LOC': 'LOCAL', 'ORG': 'ORGANIZAÇÃO', 'PER': 'PESSOA'}
+        self.modo_avalicao = modo_avaliacao
+        self.y_preds = []
+        self.textos = []
 
     def _get_map_labels(self):
         return self.map_labels
@@ -17,10 +22,52 @@ class SpacyNER(NER):
         retorno = []
         if type(texto) != float:
             doc = self.nlp(texto)
+
+            # Salva o resultado para futura medição de desempenho
+            if self.modo_avalicao:
+                self.y_preds.append(doc.ents)
+                self.textos.append(texto)
+
             for ent in doc.ents:
                 retorno.append((''.join(str(ent)), self._get_map_labels()[ent.label_]))
 
         return retorno
+
+    def avaliar(self):
+        self.calcular_y_true()
+        print(self.y_true)
+
+    def calcular_y_true(self):
+        # Recupera os labels verdadeiros para posterior avaliação de desempenho dos modelos.
+        self.y_true = []
+        self.golds = []
+
+        with jsonlines.open('labeled_4_labels.jsonl') as reader:
+            for obj in reader:
+                doc = self.nlp.tokenizer(obj['text'])
+                labels = obj['labels']
+                y_true_obj = []
+
+                for token in doc:
+                    inicio = token.idx
+                    fim = inicio + len(token)
+                    tipo = 'O'
+
+                    for label in labels:
+                        inicio_label = label[0]
+                        fim_label = label[1]
+                        cat = label[2]
+
+                        if inicio >= inicio_label and fim <= fim_label:
+                            tipo = cat
+                            break
+
+                    #y_true_obj.append((tipo, token))
+                    y_true_obj.append(tipo)
+
+                self.y_true.append(y_true_obj)
+                self.golds.append(GoldParse(doc, entities=y_true_obj))
+
 
 
 if __name__ == '__main__':
